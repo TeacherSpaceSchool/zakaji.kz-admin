1a8cf51f584755ac535771234fb8dd2d01294fac
TeacherSpaceSchool <azyk.store@gmail.com>
2023-10-12 00:54:59 +0600

    check

--- src/lib.js
+++ src/lib.js
@@ -15,10 +15,10 @@ export const getJWT = (cookie)=>{
         }
         if (c.indexOf(name) === 0) {
             let jwt = c.substring(name.length, c.length)
-            if(process.browser&&!sessionStorage.extended) {
+            /*if(process.browser&&!sessionStorage.extended) {
                 sessionStorage.extended = true
                 document.cookie = `jwt=${jwt};expires=Sun, 31 May 2048 12:35:23 GMT;path=/;SameSite=Lax;secure=true`;
-            }
+            }*/
             //console.timeEnd('perfomance')
             return jwt;
         }
